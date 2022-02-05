15146363e3ce1818d241cf4c69e39fa452832aa6
dtt <t2dx.inc@gmail.com>
2022-02-05 23:53:34 +0700

    add cors *

--- src/main/java/com/datmt/fake_api/controller/CatController.java
+++ src/main/java/com/datmt/fake_api/controller/CatController.java
@@ -1,22 +1,17 @@
 package com.datmt.fake_api.controller;
 
 
-
 import com.datmt.fake_api.helpers.Cavatar;
 import com.datmt.fake_api.model.Cat;
 import com.datmt.fake_api.repository.CatRepository;
 import com.github.javafaker.Faker;
-import com.talanlabs.avatargenerator.Avatar;
-import com.talanlabs.avatargenerator.cat.CatAvatar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +25,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("cats")
 public class CatController {
 
